ed0ab1a19d7e073a3aade67ac2b0d8b836656cb7
Matthew Riggott <Matthew.S.Riggott@gmail.com>
2015-04-05 13:56:31 -0400

    working version

--- code.rb
+++ code.rb
@@ -1,1 +1,12 @@
-#YOUR CODE GOES HERE
+COINS = [1, 5, 10, 25, 50, 100]
+#lets go greedy
+def make_change(total, *change)
+  COINS.reverse.each do |coin|
+    if total == 0
+      return change
+    elsif total >= coin
+      return make_change(total - coin, change.flatten, coin)
+    end
+  end
+end
+puts make_change(72)
